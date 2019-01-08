1215a815cd2e9e747781d110e58db295e1730a3f
kelvinrandu <randukelvin@gmail.com>
2019-01-08 15:19:12 +0300

    updated missing files

--- TestChain/Block.h
+++ TestChain/Block.h
@@ -1,44 +1,23 @@
 #include <cstdint>
 #include <iostream>
-	
+#include <sstream>
+
 using namespace std;
 
 class Block {
 public:
+    string sHash;
     string sPrevHash;
 
     Block(uint32_t nIndexIn, const string &sDataIn);
-
-    string GetHash();
 
     void MineBlock(uint32_t nDifficulty);
 
 private:
     uint32_t _nIndex;
-    int64_t _nNonce;
+    uint32_t _nNonce;
     string _sData;
-    string _sHash;
     time_t _tTime;
 
-    string _CalculateHash() const;
-};
-
-class Block {
-public:
-    string sPrevHash;
- 
-    Block(uint32_t nIndexIn, const string &sDataIn);
- 
-    string GetHash();
- 
-    void MineBlock(uint32_t nDifficulty);
- 
-private:
-    uint32_t _nIndex;
-    int64_t _nNonce;
-    string _sData;
-    string _sHash;
-    time_t _tTime;
- 
     string _CalculateHash() const;
 };
